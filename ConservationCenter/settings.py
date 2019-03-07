fb9002098059061c5c6f6388563ccb709ba5feda
liuhuan <18737307883@sina.cn>
2019-03-07 10:24:12 +0800

    modify settings

--- ConservationCenter/settings.py
+++ ConservationCenter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '06h2p#u+^=mm3g2_s2r+j(l5pwsihyvg_!=-z(xpe-c%p2jk@z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
